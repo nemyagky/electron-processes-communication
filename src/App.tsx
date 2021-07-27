1ccc0514bdac42fb9ea67551ab59c54d876affd1
myagky <ivan.mag.web@gmail.com>
2021-07-27 22:10:29 +0300

    Architecture Refactoring

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {MovableBox} from "./components/MovableBox/movable-box";
-
-export const App: React.FC = () => (
-    <MovableBox />
-);
--- src/renderer.ts
+++
@@ -1,2 +0,0 @@
-import '../public/index.css';
-import './index';
